4b7d277926c2ce4811e50cd4dde76b5720b27856
SeaiiaeS <hayk.adamyan.2002@gmail.com>
2022-06-17 15:40:34 +0400

    font task added

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,14 @@ import { clean } from './gulp/task/clean.js';
 import { pug } from './gulp/task/pug.js';
 import { scss } from './gulp/task/scss.js';
 import { image } from './gulp/task/image.js';
+import { fonts } from './gulp/task/fonts.js';
 import { browserSync } from './gulp/config/server.js';
 
 gulp.task('watch', () => {
     gulp.watch(app.path.watch.pug, pug);
     gulp.watch(app.path.watch.scss, scss);
     gulp.watch(app.path.watch.image, image);
+    gulp.watch(app.path.watch.fonts, fonts);
 })
 
 const defaultTask = gulp.series(
@@ -26,7 +28,7 @@ const defaultTask = gulp.series(
     gulp.parallel(
         pug,
         scss,
-        // fonts,
+        fonts,
         image
     ),
     gulp.parallel('watch' , browserSync)
